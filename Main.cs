5df5fd4b2fe34eb8eb6fad83715948d2fe165851
adielbm <adiel.bm5@gmail.com>
2024-08-28 02:20:49 +0300

    more

--- Main.cs
+++ Main.cs
@@ -5,153 +5,171 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Flow.Launcher.Plugin;
+using OpenMeteo;
+using System.Threading;
+using System.Windows.Controls;
+using System.IO;
 
 namespace Flow.Launcher.Plugin.Weather
 {
-    public class Main : IPlugin
+    public class Main : IAsyncPlugin // , ISettingProvider
     {
         private string ClassName => GetType().Name;
-
         private PluginInitContext _context;
-        private readonly HttpClient _httpClient;
-        private const string API_URL = "https://api.open-meteo.com/v1/forecast?latitude=31.769&longitude=35.2163&daily=weather_code,apparent_temperature_max,apparent_temperature_min&wind_speed_unit=ms&timezone=Africa%2FCairo&forecast_days=1";
+        // private Settings _settings;
+
+        // public Control CreateSettingPanel()
+        // {
+        //     return new WeatherSettings(_settings);
+        // }
+
+
+        private readonly OpenMeteoClient _client;
         public Main()
         {
-            _httpClient = new HttpClient();
+            _client = new OpenMeteoClient();
         }
 
-        public void Init(PluginInitContext context)
+        public Task InitAsync(PluginInitContext context)
         {
             _context = context;
+            return Task.CompletedTask;
         }
 
-
-        private async Task<WeatherData> GetWeatherAsync()
+        public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
         {
+            if (string.IsNullOrWhiteSpace(query.Search))
+            {
+                // return empty
+                return new List<Result>
+                {
+                    new Result
+                    {
+                        Title = "Please enter a city name",
+                        IcoPath = "Images\\weather-icon.png"
+                    }
+                };
+            }
             try
             {
-                var response = await _httpClient.GetAsync(API_URL);
-                response.EnsureSuccessStatusCode();
-                var jsonString = await response.Content.ReadAsStringAsync();
-                _context.API.LogInfo(ClassName, "[[jsonString]]" + jsonString);
+                token.ThrowIfCancellationRequested();
 
-                var options = new JsonSerializerOptions
+                // Get city data
+                GeocodingOptions geocodingData = new GeocodingOptions(query.Search);
+                var geocodingResult = await _client.GetLocationDataAsync(geocodingData);
+
+                if (geocodingResult?.Locations == null)
                 {
-                    PropertyNameCaseInsensitive = true
+                    return new List<Result>
+                        {
+                            new Result
+                            {
+                                Title = "City not found",
+                                SubTitle = "Please check the city name and try again",
+                                IcoPath = "Images\\weather-icon.png"
+                            }
+                        };
+                }
+
+                var cityData = geocodingResult.Locations[0];
+
+                token.ThrowIfCancellationRequested();
+
+                // Set custom options
+                WeatherForecastOptions options = new WeatherForecastOptions
+                {
+                    Temperature_Unit = TemperatureUnitType.celsius
+                    // Temperature_Unit = _settings?.TemperatureUnit == "Celsius" ? TemperatureUnitType.celsius : TemperatureUnitType.fahrenheit,
                 };
 
-                WeatherData weatherData = JsonSerializer.Deserialize<WeatherData>(jsonString, options) ?? throw new Exception("Failed to deserialize WeatherInfo");
-                return weatherData;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception in GetWeatherAsync: {ex}");
-                throw;
-            }
-        }
+                if (cityData?.Name != null)
+                {
+                    WeatherForecast weatherData = await _client.QueryAsync(cityData?.Name/* , options */);
 
-        public List<Result> Query(Query query)
-        {
-            if (!string.IsNullOrWhiteSpace(query.Search))
+                    // _context.API.ShowMsgError(ClassName, $"WEATHER: c:{cityData.Latitude}:{cityData.Longitude}; w:{weatherData.Latitude}:{weatherData.Longitude}; {weatherData?.Daily?.Apparent_temperature_max[0]}, {weatherData?.Current?.Apparent_temperature}");
+
+                    token.ThrowIfCancellationRequested();
+
+                    if (weatherData != null)
+                    {
+                        return new List<Result>
+                            {
+                                new Result
+                                {
+                                    Title = $"{weatherData?.Current?.Apparent_temperature}°C",
+                                    SubTitle = $"{cityData.Name}, {cityData.Country}",
+                                    IcoPath = "Images\\weather-icon.png"
+                                }
+                            };
+
+                    }
+
+                }
+
+            }
+            catch (OperationCanceledException)
             {
                 // return empty
                 return new List<Result>();
             }
-
-            try
-            {
-                WeatherData weatherData = GetWeatherAsync().Result ?? throw new Exception("weatherData is null");
-
-                return new List<Result>
-                {
-                    new Result
-                    {
-                        Title = $"Weather in Jerusalem",
-                        SubTitle = $"Max: {weatherData.Daily.ApparentTemperatureMax[0]}°C, Min: {weatherData.Daily.ApparentTemperatureMin[0]}°C",
-                        IcoPath = "Images\\weather-icon.png"
-                    }
-                };
-            }
             catch (Exception ex)
             {
-                Console.WriteLine($"Exception in Query: {ex}");
                 return new List<Result>
                 {
-                    new Result
-                    {
-                        Title = "Error fetching weather",
-                        SubTitle = $"Details: {ex.Message}",
+                    new() {
+                        Title = $"err: {ex.Message}, {ex.Source}",
+                        SubTitle = $"{ex.InnerException}, {ex.StackTrace}",
                         IcoPath = "Images\\weather-icon.png"
                     }
                 };
             }
+            return new List<Result>();
         }
-
-        public class WeatherData
-        {
-            [JsonPropertyName("latitude")]
-            public double Latitude { get; set; }
-
-            [JsonPropertyName("longitude")]
-            public double Longitude { get; set; }
-
-            [JsonPropertyName("generationtime_ms")]
-            public double GenerationTimeMs { get; set; }
-
-            [JsonPropertyName("utc_offset_seconds")]
-            public int UtcOffsetSeconds { get; set; }
-
-            [JsonPropertyName("timezone")]
-            public string Timezone { get; set; }
-
-            [JsonPropertyName("timezone_abbreviation")]
-            public string TimezoneAbbreviation { get; set; }
-
-            [JsonPropertyName("elevation")]
-            public double Elevation { get; set; }
-
-            [JsonPropertyName("daily_units")]
-            public DailyUnits DailyUnits { get; set; }
-
-            [JsonPropertyName("daily")]
-            public Daily Daily { get; set; }
-        }
-
-        public class DailyUnits
-        {
-            [JsonPropertyName("time")]
-            public string Time { get; set; }
-
-            [JsonPropertyName("weather_code")]
-            public string WeatherCode { get; set; }
-
-            [JsonPropertyName("apparent_temperature_max")]
-            public string ApparentTemperatureMax { get; set; }
-
-            [JsonPropertyName("apparent_temperature_min")]
-            public string ApparentTemperatureMin { get; set; }
-        }
-
-        public class Daily
-        {
-            [JsonPropertyName("time")]
-            public List<string> Time { get; set; }
-
-            [JsonPropertyName("weather_code")]
-            public List<int> WeatherCode { get; set; }
-
-            [JsonPropertyName("apparent_temperature_max")]
-            public List<double> ApparentTemperatureMax { get; set; }
-
-            [JsonPropertyName("apparent_temperature_min")]
-            public List<double> ApparentTemperatureMin { get; set; }
-        }
-
-
 
         public void Dispose()
         {
-            _httpClient.Dispose();
+            // _httpClient.Dispose();
         }
     }
+
+    /*  public partial class WeatherSettings : UserControl
+     {
+         private readonly Settings _settings;
+         private bool _contentLoaded;
+
+         public WeatherSettings(Settings settings)
+         {
+             _settings = settings;
+             InitializeComponent();
+             this.DataContext = settings;
+         }
+
+         private void InitializeComponent()
+         {
+             if (!_contentLoaded)
+             {
+                 _contentLoaded = true;
+                 Uri resourceLocator = new Uri("/Flow.Launcher.Plugin.Weather;component/WeatherSettings.xaml", UriKind.Relative);
+                 System.Windows.Application.LoadComponent(this, resourceLocator);
+             }
+         }
+     } */
+
+
+    /*  public class Settings
+     {
+         public string TemperatureUnit { get; set; } = "Celsius";
+
+         [JsonIgnore]
+         public List<string> TemperatureUnits { get; } = new List<string> { "Celsius", "Fahrenheit" };
+     }
+
+     public static class Temperature
+     {
+         public static double CelsiusToFahrenheit(double celsius)
+         {
+             return (celsius * 9 / 5) + 32;
+         }
+     } */
+
 }
